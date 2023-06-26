61ad0f2004f4c9e72fb3bef29f167a4c05e1f20f
Shubham-Parmar01 <137475710+Shubham-Parmar01@users.noreply.github.com>
2023-06-26 10:02:36 +0530

    Update Demo.java

--- src/test/java/gitDemo/Demo.java
+++ src/test/java/gitDemo/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hiii Git....!");
-		System.out.println("hii new Project");
+		for(;;){
+			System.out.println("hii new Project");
+		}	
 	}
 }
